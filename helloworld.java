4f17339ce889d8e96374b09acb7983ec45adbf44
ttansang208 <tansangtran208@gmail.com>
2021-09-21 16:39:50 +0700

    add some script

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello World !");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap ten cua ban : ");
+        String name = sc.nextLine();
+        System.out.println("Hello " + name);
     }
 }
